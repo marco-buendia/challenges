33852e419e796c027e927f2a87a5a43bc104e68d
S2GEnergy <contacto@s2g.energy>
2020-11-11 16:46:51 -0600

    changed conenction to db

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 'use strict';
 var express = require('express');
 var bodyParser = require('body-parser');
-var knex = require('knex')({
-    client: 'pg',
-    version: '11.4',
-    connection: {
-      host : 'ec2-3-214-46-194.compute-1.amazonaws.com',
-      user : 'qdlwgooewmhflu',
-      password : 'ee8fdfbd8a9104e19451baa259bf78bb8be8f767a50d69e0d5c51e77015e6039',
-      database : 'd6t60n7r9hfjua'
-    }
-});
 const app = express();
 var jsonParser = bodyParser.json();
 
-function insertNewUSer(json){
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'qdlwgooewmhflu',
+  host: '    ec2-3-214-46-194.compute-1.amazonaws.com',
+  database: 'd6t60n7r9hfjua',
+  password: 'ee8fdfbd8a9104e19451baa259bf78bb8be8f767a50d69e0d5c51e77015e6039',
+  port: 5432,
+})
+
+function insertNewUSer(knex,data){
     console.log("hola")
-    return knex('users').insert(json);
+    pool.query('INSERT INTO users ("user_id","name","lastName","phoneNumber","userType") VALUES %s ON CONFLICT DO NOTHING');
 }
+
+
+function connect () {
+  const config = {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
+  };
+  config.host = process.env.INSTANCE_CONNECTION_NAME;
+
+  // Connect to the database
+  const knex = Knex({
+    client: 'pg',
+    connection: config
+  });
+  //console.log(config);
+  return knex;
+}
+
 
 app.get('/', function (req, res) {
   res.send("This is a server test. Hello");
@@ -30,9 +48,17 @@ app.post('/user', jsonParser, function (req, res, next) {
     var jsn = JSON.stringify(req.body);
     var jsondata = JSON.parse(jsn);
 
-    console.log(jsondata)
+    const data = []
 
-    insertNewUSer(jsondata).then(() => {
+    data.push("");
+    data.push(jsondata["name"]);
+    data.push(jsondata["lastName"]);
+    data.push(jsondata["phoneNumber"]);
+    data.push(jsondata["userType"]);
+
+    const knex = connect();
+
+    insertNewUSer(knex,data).then(() => {
         res
           .status(200)
           .set('Content-Type', 'text/plain')
