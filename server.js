17c8dd9baf7264347245fb83f43d19ec812b1de4
S2GEnergy <contacto@s2g.energy>
2020-11-11 18:39:00 -0600

    test user_id

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/user', function (req,res){
       if (error) {
         throw error
       }
-      res.send(response.status(200).json(results.rows))
+      console.log(results.rows[0])
       
     })
   }
